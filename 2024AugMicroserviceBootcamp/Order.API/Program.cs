027fc9b16178d1fbb2d426210d1ae2589749a6ba
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2025-02-10 17:25:05 +0300

    Send rabbitmq producer

--- 2024AugMicroserviceBootcamp/Order.API/Program.cs
+++ 2024AugMicroserviceBootcamp/Order.API/Program.cs
@@ -1,3 +1,5 @@
+using ServiceBus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<BusOption>(builder.Configuration.GetSection(nameof(BusOption)));
 
 var app = builder.Build();
 
---
+++ 2024AugMicroserviceBootcamp/ServiceBus/Bus.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ServiceBus
+{
+    public class Bus(IOptions<BusOption> busOptions) : IBus
+    {
+        public  Task Send<T>(T message, string exchangeName) where T : class
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(busOptions.Value.Url)
+            };
+
+            using var connection =  connectionFactory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.ConfirmSelect();
+
+            //create exchange
+            //string exchange = "orderapi.order.created.event.exchange";
+            channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+            var messageBody = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+            var properties = channel.CreateBasicProperties();
+
+
+            channel.BasicPublish(
+                                exchange: exchangeName,
+                                routingKey: "", // Fanout Exchange kullanıyorsan routing key boş olmalı
+                                basicProperties: null, // Boş bir BasicProperties nesnesi oluştur
+                                body: messageBody
+            );
+            channel.WaitForConfirms(TimeSpan.FromMinutes(1));
+
+            return Task.CompletedTask; 
+        }
+    }
+}
